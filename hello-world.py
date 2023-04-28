f989c9201fe78959b4b8b9ed5ec6c9f629db993f
sam078 <saimrao849@gmail.com>
2023-04-28 11:05:09 -0700

    Initial commit

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+from math import *   #import math library for use all math function
+print('start Saim, this is gona be final decision. start from',0)
+name = 'Saim'
+age = 24
+gender = 'male'
+
+print('my name is', name.upper() ,'\nmy age is', age, '\nI am ', gender)
+print(sqrt(age) , max(age , 45) , round(882.55))
+# numbers in python 
+print(int(sqrt(10)))
